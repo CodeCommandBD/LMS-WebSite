01fcb455eaef2bb3c4331f49a73ece65f6d456e2
shanto <shantokumar22@gmail.com>
2026-02-08 16:21:48 +0600

    Implement user authentication controllers for registration, login, and logout.

--- server/Controller/user.contollers.js
+++ server/Controller/user.contollers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 export const registerUser = async (req, res) => {
@@ -21,6 +22,43 @@ export const registerUser = async (req, res) => {
 
        return res.status(201).json({ success: true, message: "Account created successfully" });
 
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password){
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({ success: false, message: "Incorrect email or password" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({ success: false, message: "Incorrect email or password" });
+        }
+
+        // token generation 
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        // set cookie
+        return res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "strict", maxAge: 60 * 60 * 1000 }).json({ success: true, message: "Login successful", user, token });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+
+export const logoutUser = async (req, res) => {
+    try {
+        return res.status(200).clearCookie("token").maxAge(0).json({ success: true, message: "Logout successful" });
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message });
     }
