bd7db05606e6da7a3091d3b7eedb046c28c16af4
shanto <shantokumar22@gmail.com>
2026-02-08 15:27:17 +0600

    establish initial Express server application with environment configuration, database connection, and user routing module.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import app from "./app.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000 
 
 
