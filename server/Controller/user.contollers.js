711e270ccf5960de7bd8b24c1e0feb0ba534ae63
shanto <shantokumar22@gmail.com>
2026-02-08 15:23:22 +0600

    Implement user registration controller with validation.

--- server/Controller/user.contollers.js
+++ server/Controller/user.contollers.js
@@ -4,11 +4,17 @@ export const registerUser = async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
         if(!name || !email || !password || !role){
-            return res.status(400).json({ message: "All fields are required" });
+            return res.status(400).json({ success: false, message: "All fields are required" });
         }
-        const user = await User.create({ name, email, password, role });
-        res.status(201).json({ message: "User created successfully", user });
+       const user = await User.findOne({email});
+       if(user){
+           return res.status(400).json({ success: false, message: "User already exists with this email" });
+       }
+
+       await User.create({ name, email, password, role });
+       return res.status(201).json({ success: true, message: "User created successfully" });
+
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ success: false, message: error.message });
     }
 }
