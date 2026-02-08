6d0a5a7bc44477ee26593d107f54771c1c9a60b9
shanto <shantokumar22@gmail.com>
2026-02-08 15:55:04 +0600

    Fix .gitignore files to properly ignore node_modules and add comprehensive ignore patterns

--- server/Controller/user.contollers.js
+++ server/Controller/user.contollers.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
 
 export const registerUser = async (req, res) => {
     try {
@@ -6,12 +8,17 @@ export const registerUser = async (req, res) => {
         if(!name || !email || !password || !role){
             return res.status(400).json({ success: false, message: "All fields are required" });
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        // check if user already exists
        const user = await User.findOne({email});
        if(user){
            return res.status(400).json({ success: false, message: "Account already exists with this email" });
        }
 
-       await User.create({ name, email, password, role });
+       await User.create({ name, email, password: hashedPassword, role });
+
        return res.status(201).json({ success: true, message: "Account created successfully" });
 
     } catch (error) {
