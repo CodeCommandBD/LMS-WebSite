12de0845da8e23fa88a76cfab852bc629d71b57f
shanto <shantokumar22@gmail.com>
2026-02-12 01:24:40 +0600

    Add initial `UpdateCourse` page component for admin.

--- client/src/Pages/admin/UpdateCourse.jsx
+++ client/src/Pages/admin/UpdateCourse.jsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Link } from 'react-router'
 
 const UpdateCourse = () => {
   return (
-    <div>UpdateCourse</div>
+    <div className='md:p-10 p-4'>
+        <div className='w-full bg-white flex items-center justify-between mb-5'>
+            <h1 className='text-2xl font-bold'>Add details information of course</h1>
+            <Link to="/admin/courses" className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg'>
+               <Button className='cursor-pointer'>Go to lectures page</Button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
