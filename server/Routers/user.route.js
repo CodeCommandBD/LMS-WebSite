010f77bd99a67d5ed9df4e5a520678c5d4795208
shanto <shantokumar22@gmail.com>
2026-02-25 22:30:13 +0600

    update user routes

--- server/Routers/user.route.js
+++ server/Routers/user.route.js
@@ -6,8 +6,14 @@ import {
   getCurrentUser,
   updateProfile,
   getInstructorProfile,
+  forgotPassword,
+  resetPassword,
+  getEnrolledCourses,
+  getWishlistCourses,
+  getAllUsers,
 } from "../Controller/user.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
+import { authorize } from "../middleware/authorize.middleware.js";
 import { uploadProfilePicture } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
@@ -15,7 +21,12 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 router.get("/me", authenticate, getCurrentUser);
+router.get("/enrolled-courses", authenticate, getEnrolledCourses);
+router.get("/wishlist", authenticate, getWishlistCourses);
+router.get("/", authenticate, authorize("admin"), getAllUsers);
 // Public: get instructor profile
 router.get("/instructor/:instructorId", getInstructorProfile);
 
