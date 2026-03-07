9a455e991081b309d975cb4e3f1a2c2bc182e7c6
shanto <shantokumar22@gmail.com>
2026-03-07 23:14:19 +0600

    added catch-all route to serve client index.html from dist folder

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ import reviewRouter from "./Routers/review.route.js";
 import categoryRouter from "./Routers/category.route.js";
 import blogRouter from "./Routers/blog.route.js";
 import settingsRouter from "./Routers/settings.route.js";
+import statsRouter from "./Routers/stats.route.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -132,6 +133,7 @@ app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/settings", settingsRouter);
+app.use("/api/v1/stats", statsRouter);
 
 // Static files & Catch-all route (MUST be at the end)
 // In local dev: serves from client/dist after running the build
